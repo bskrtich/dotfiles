23327545a4261b12c7f0772ecc19a5cb738f0132
Brandon Skrtich <bskrtich@gmail.com>
2018-01-23 14:55:18 -0700

    Add more apps

--- bin/part02.sh
+++ bin/part02.sh
@@ -12,8 +12,32 @@ yellow() { printf "\e[1;33m%b\e[0m\n" "$@"; }
 red() { printf "\e[1;31m%b\e[0m\n" "$@"; }
 
 # Install Homebrew apps
+green "Install Homebrew apps"
+brew install git wget vim python3 jq ack bash composer dnsmasq
+
 
 # Install Homebrew-Cask apps
+brew cask install sublime-text
+brew cask install discord
+brew cask install vmware-fusion
+brew cask install vlc
+brew cask install spotify
+brew cask install postman
+brew cask install Kaleidoscope
+brew cask install iterm2
+brew cask install grandperspective
+brew cask install 1password
+brew cask install slack
+brew cask install application-loader
 
 # Install mas apps
+mas install 418423076 # YemuZip
+mas install 405580712 # StuffIt Expander
+mas install 407963104 # Pixelmator
+mas install 497799835 # Xcode
+mas install 525372278 # UTC Bar
 
+# Missing applications
+yellow "Oracle SQL Developer"
+yellow "Xojo"
+yellow "Lock Me Now"
