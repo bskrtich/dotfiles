f5eb3063e4c2c3c1bbf9d86a317d56f9599d7a1d
Brandon Skrtich <bskrtich@gmail.com>
2018-01-24 13:32:12 -0700

    Fix setup.sh script

--- setup.sh
+++ setup.sh
@@ -10,11 +10,23 @@ green() { printf "\e[1;32m%b\e[0m\n" "$@"; }
 yellow() { printf "\e[1;33m%b\e[0m\n" "$@"; }
 red() { printf "\e[1;31m%b\e[0m\n" "$@"; }
 
+# ref: https://askubuntu.com/a/30157/8698
+if ! [ $(id -u) = 0 ]; then
+   echo "The script need to be run as root." >&2
+   exit 1
+fi
+
+if [ $SUDO_USER ]; then
+    real_user=$SUDO_USER
+else
+    real_user=$(whoami)
+fi
+
 # Install Homebrew
 #  Package manager for macOS
 # https://brew.sh/
 green "Installing Homebrew"
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sudo -u $real_user /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 green "Installing git"
 brew install git
